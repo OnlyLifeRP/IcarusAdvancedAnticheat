2a488bb181b5417cea456b903765aca70ead2ade
Thomthom160 <89802398+Thomthom160@users.noreply.github.com>
2024-08-03 04:37:39 +0200

    modified:   src/commands/ScreenshotCommand.ts 	modified:   src/commands/WipeEntitiesCommand.ts

--- src/commands/WipeEntitiesCommand.ts
+++ src/commands/WipeEntitiesCommand.ts
@@ -2,7 +2,7 @@ import { Command } from "../core/Command";
 
 export class WipeEntitiesCommand extends Command {
 	constructor() {
-		super("entity_wipe", "Removes all networked entities", []);
+		super("entityWipe", "Removes all networked entities", []);
 	}
 
 	/**
